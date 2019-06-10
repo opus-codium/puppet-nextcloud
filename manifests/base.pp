120de435c1b3d789163358dcd0a9a696ac6ace4d
Manuel Tancoigne <m.tancoigne@gmail.com>
2019-06-10 15:33:33 +0200

    Implement "nextcloud::base" class

---
+++ manifests/base.pp
@@ -0,0 +1,47 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include nextcloud::base
+class nextcloud::base {
+  include nextcloud
+
+  user { $nextcloud::user:
+    ensure     => present,
+    home       => $nextcloud::base_dir,
+    system     => true,
+    managehome => true,
+  }
+
+  file { $nextcloud::base_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  -> file { $nextcloud::persistent_data_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0751',
+  }
+  -> file { $nextcloud::data_dir:
+    ensure => directory,
+    owner  => $nextcloud::user,
+    group  => $nextcloud::group,
+    mode   => '0750',
+  }
+  -> file { $nextcloud::config_dir:
+    ensure => directory,
+    owner  => $nextcloud::user,
+    group  => $nextcloud::group,
+    mode   => '0750',
+  }
+  -> file { $nextcloud::config_main_file:
+    ensure => file,
+    owner  => $nextcloud::user,
+    group  => $nextcloud::group,
+    mode   => '0640',
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,22 @@
 #   include nextcloud
 class nextcloud (
   String[1] $hostname,
+  String[1] $user      = 'nextcloud',
+  String[1] $group     = $user,
 ){
   $base_dir            = "/srv/www/${hostname}"
   $persistent_data_dir = "${base_dir}/persistent-data"
   $data_dir            = "${persistent_data_dir}/data"
   $config_dir          = "${persistent_data_dir}/config"
+  $config_main_file    = "${config_dir}/main.php"
 
   contain nextcloud::database
   contain nextcloud::dependencies
+  contain nextcloud::base
+  contain nextcloud::install
+
+  Class['nextcloud::dependencies']
+  -> Class['nextcloud::database']
+  -> Class['nextcloud::base']
+  -> Class['nextcloud::install']
 }
