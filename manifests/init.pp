e50e4d5d1a322e6097500d54ad9d4540517efb26
Manuel Tancoigne <m.tancoigne@gmail.com>
2019-06-10 11:28:49 +0200

    Create "nextcloud" class

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include nextcloud
+class nextcloud (
+  String[1] $hostname,
+){
+  $base_dir            = "/srv/www/${hostname}"
+  $persistent_data_dir = "${base_dir}/persistent-data"
+  $data_dir            = "${persistent_data_dir}/data"
+  $config_dir          = "${persistent_data_dir}/config"
+
+}
