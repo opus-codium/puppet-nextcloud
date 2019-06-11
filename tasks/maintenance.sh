43e0487d6a1e3248090a149a6d23efef1a34b120
Romuald Conty <romuald@opus-codium.fr>
2019-06-11 10:31:33 +0200

    Implement "maintenance" task

---
+++ tasks/maintenance.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+error () {
+  echo $1 >&2
+  exit 1
+}
+
+nextcloud_path=$(/opt/puppetlabs/bin/facter nextcloud.path)
+
+[ -z $nextcloud_path ] && error 'Nextcloud is not installed on this node.'
+
+nextcloud_user=$(/usr/bin/stat -c '%U' "${nextcloud_path}/current/.htaccess")
+
+occ_maintenance_option=''
+[ -n $PT_mode ] && occ_maintenance_option="--${PT_mode}"
+
+cd $nextcloud_path/current && sudo -u nextcloud /usr/bin/php occ maintenance:mode $occ_maintenance_option
