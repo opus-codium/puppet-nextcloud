1da77341fb1e24a70e13ba5c3b6b9d953a556bc5
Romuald Conty <romuald@opus-codium.fr>
2019-07-27 14:19:39 +0200

    Add new task to check for updates

---
+++ tasks/updatecheck.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+error () {
+  echo $1 >&2
+  exit 1
+}
+
+nextcloud_path=$(/opt/puppetlabs/bin/facter nextcloud.path)
+
+[ -z $nextcloud_path ] && error 'Nextcloud is not installed on this node.'
+
+nextcloud_user=$(/usr/bin/stat -c '%U' "${nextcloud_path}/current/.htaccess")
+
+cd $nextcloud_path/current && sudo -u $nextcloud_user /usr/bin/php occ update:check
