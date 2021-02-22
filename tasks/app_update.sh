a4169e71ca0b7a5cd9e620d1d6649b1cbee6705f
Romuald Conty <romuald@opus-codium.fr>
2021-02-22 14:41:03 +0100

    Add a new task to update all apps

---
+++ tasks/app_update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -xe
+
+error () {
+  echo $1 >&2
+  exit 1
+}
+
+[ -x /usr/local/bin/occ ] || error 'Nextcloud is not installed on this node.'
+
+# Grab user and path used by Nextcloud
+nextcloud_path=$(sudo /opt/puppetlabs/bin/facter nextcloud.path)
+
+# Module files location
+files_dir="$(dirname $0)/../files"
+
+# Target file in Nextcloud to overwrite current config
+config_overwrite_file="${nextcloud_path}/current/config/z-overwrite.config.php"
+
+# Copy it from module to Nextcloud sources
+cat "${files_dir}/enable-appstore.config.php" | sudo tee $config_overwrite_file > /dev/null
+
+# The output
+/usr/local/bin/occ app:update --all
+
+# Clean the config overwrite
+sudo rm $config_overwrite_file
