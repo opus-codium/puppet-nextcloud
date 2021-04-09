08ad9ca506bf8bff253a3b54ee6f8c870d0704d5
Romain Tartière <romain@blogreen.org>
2021-04-09 08:18:14 -1000

    Look for PT__task environment variable
    This variable contains the current task name when running Bolt tasks.
    When this variable is set, we are in the context of a Bolt task, so
    addons management should be allowed.

--- tasks/app_update.sh
+++ tasks/app_update.sh
@@ -9,20 +9,4 @@ error () {
 
 [ -x /usr/local/bin/occ ] || error 'Nextcloud is not installed on this node.'
 
-# Grab user and path used by Nextcloud
-nextcloud_path=$(sudo /opt/puppetlabs/bin/facter nextcloud.path)
-
-# Module files location
-files_dir="${PT__installdir}/nextcloud/files"
-
-# Target file in Nextcloud to overwrite current config
-config_overwrite_file="${nextcloud_path}/current/config/z-overwrite.config.php"
-
-# Copy it from module to Nextcloud sources
-sudo tee $config_overwrite_file < "${files_dir}/enable-appstore.config.php" > /dev/null
-
-# The output
 /usr/local/bin/occ app:update --all
-
-# Clean the config overwrite
-sudo rm $config_overwrite_file
