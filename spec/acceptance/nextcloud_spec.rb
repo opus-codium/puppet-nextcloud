4d7f82a1e8f8251a2d162fc050e1549362bcfcca
Romain Tartière <romain@blogreen.org>
2024-06-07 16:23:23 -1000

    Make sure /srv directory exist for CI
    FHS 3.0 define /srv but some versions of Debian do not include it.  Make
    sure it exist.  This is however probably a terrible idea and we should
    rather consider installing in a directory we are sure will exist.

--- spec/acceptance/nextcloud_spec.rb
+++ spec/acceptance/nextcloud_spec.rb
@@ -5,6 +5,14 @@ require 'spec_helper_acceptance'
 describe 'nextcloud class' do
   it 'works idempotently with no errors' do
     pp = <<~MANIFEST
+      # FIXME: Some distro don't have this directory.  Change it to something more stable?
+      file { '/srv/www':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+
       class { 'nextcloud':
         hostname          => 'nextcloud.example.com',
         database_password => 'the super secret password used by postgresql',
