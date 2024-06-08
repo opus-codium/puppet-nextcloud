7579aa078ca49fc3ab7cfd10cfeaef25687c180f
Romain Tartière <romain@blogreen.org>
2024-06-07 17:18:12 -1000

    Fix dependencies installation with PHP 8.0+
    Debian used to package the json module in its own packages, but recent
    versions of PHP include it in the core package and there is no
    corresponding -json package.
    
    Skip this bogus packages when the PHP version indicate a recent version
    of PHP.

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -6,24 +6,44 @@ class nextcloud::dependencies (
 ) {
   assert_private()
 
+  $php_packages = if versioncmp($nextcloud::php_version, '8.0') >= 0 {
+    [
+      "php${nextcloud::php_version}",
+      "php${nextcloud::php_version}-bcmath",
+      "php${nextcloud::php_version}-bz2",
+      "php${nextcloud::php_version}-curl",
+      "php${nextcloud::php_version}-gd",
+      "php${nextcloud::php_version}-gmp",
+      "php${nextcloud::php_version}-intl",
+      "php${nextcloud::php_version}-mbstring",
+      "php${nextcloud::php_version}-pgsql",
+      "php${nextcloud::php_version}-xml",
+      "php${nextcloud::php_version}-zip",
+    ]
+  } else {
+    [
+      "php${nextcloud::php_version}",
+      "php${nextcloud::php_version}-bcmath",
+      "php${nextcloud::php_version}-bz2",
+      "php${nextcloud::php_version}-curl",
+      "php${nextcloud::php_version}-gd",
+      "php${nextcloud::php_version}-gmp",
+      "php${nextcloud::php_version}-intl",
+      "php${nextcloud::php_version}-json",
+      "php${nextcloud::php_version}-mbstring",
+      "php${nextcloud::php_version}-pgsql",
+      "php${nextcloud::php_version}-xml",
+      "php${nextcloud::php_version}-zip",
+    ]
+  }
+
   $requirements = [
     'bzip2',
     $libmagickcore_extra_package_name,
     'php-apcu',
     'php-imagick',
-    "php${nextcloud::php_version}",
-    "php${nextcloud::php_version}-bcmath",
-    "php${nextcloud::php_version}-bz2",
-    "php${nextcloud::php_version}-curl",
-    "php${nextcloud::php_version}-gd",
-    "php${nextcloud::php_version}-gmp",
-    "php${nextcloud::php_version}-intl",
-    "php${nextcloud::php_version}-json",
-    "php${nextcloud::php_version}-mbstring",
-    "php${nextcloud::php_version}-pgsql",
-    "php${nextcloud::php_version}-xml",
-    "php${nextcloud::php_version}-zip",
-  ]
+  ] + $php_packages
+
   package { $requirements:
     ensure => installed,
   }
